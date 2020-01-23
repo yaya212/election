e4254306e6ae71b67de23c94d6239a41d0d6deb7
yaya212 <yaya212@users.noreply.github.com>
2020-01-23 00:40:40 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -2,41 +2,70 @@
 #include<cs50.h>
 #include<ctype.h>
 #include<math.h>
+int count_letter(string x, int y);
+int count_word(string x, int y);
+int count_sentence(string x, int y);
+void get_grade(int k, int l, int n);
 int main(void)
 {
     string s = get_string("Text: ");
-    int letter = 0;
-    int words = 0;
-    int sentence = 0;
     int l = 0;
     while(s[l] != '\0')
     {
         l++;
     }
-    for(int i = 0; i < l; i++)
+    int letter = count_letter(s,l);
+    int words = count_word(s,l);
+    int sentence = count_sentence(s,l);
+    get_grade(letter, words, sentence);
+}
+int count_letter(string x, int y)
+{
+    int letter = 0;
+    for(int i = 0; i < y; i++)
     {
-        if(isalpha(s[i]))
+        if(isalpha(x[i]))
         {
             letter++;
         }
-        if(s[i] == ' ')
+    }
+    return letter;
+}
+int count_word(string x, int y)
+{
+    int words = 0;
+    for(int i = 0; i < y; i++)
+    {
+        if(x[i] == ' ')
         {
             words++;
         }
         else
         {
-            if(i == l-1)
+            if(i == y-1)
             {
                 words++;
             }
         }
-        if(s[i] == '.' || s[i] == '!' || s[i] == '?')
+    }
+    return words;
+}
+int count_sentence(string x, int y)
+{
+    int sentence = 0;
+    for(int i = 0; i < y; i++)
+    {
+        if(x[i] == '.' || x[i] == '!' || x[i] == '?')
         {
             sentence++;
         }
     }
-    float L = (letter * 100) / words;
-    float S = (sentence * 100) / words;
+    return sentence;
+}
+void get_grade(int k, int l, int m)
+{
+    float L = ((float)k * 100) / l;
+    float S = ((float)m * 100) / l;
     int index = round((0.0588 * L) - (0.296 * S) - 15.8);
     if (index >= 16)
     {
@@ -50,5 +79,4 @@ int main(void)
     {
         printf("Grade %d\n",index);
     }
-    //printf("%d letter(s)\n%d word(s)\n%d sentence(s)\n",letter,words,sentence);
 }
