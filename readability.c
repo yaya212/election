f4a7d7f419f017fe787dc7eadc49e8d3ca235188
yaya212 <yaya212@users.noreply.github.com>
2020-01-29 13:10:10 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -4,82 +4,76 @@
 #include<ctype.h>
 #include<math.h>
 #include<string.h>
-//function responsible for counting the number of letters
-int count_letter(string x, int y);
 //function responsible for counting the number of words
-int count_word(string x, int y);
-//function responsible for counting the number of sentences
-int count_sentence(string x, int y);
+void count(string x, int y, int *letter, int *words, int *sentence);
 //function responsible for doing the calculation to determine the grade level
-void get_grade(int k, int l, int n);
+void get_grade(int *k, int *l, int *n);
 int main(void)
 {
     string s = get_string("Text: ");//s holds the input string
     int l = strlen(s);//l holds the length of string
-    int letter = count_letter(s, l);
-    int words = count_word(s, l);
-    int sentence = count_sentence(s, l);
-    get_grade(letter, words, sentence);
+    int letter = 0;//counter for the letters
+    int words = 0;//counter for the words
+    int sentence = 0;//counter for the sentences
+    count(s, l, &letter, &words, &sentence);
+    get_grade(&letter, &words, &sentence);
 }
 //x holds the string s
 //y holds the length of string
-int count_letter(string x, int y)
+int i = 0;
+void count(string x, int y, int *letter, int *words, int *sentence)
 {
-    int letter = 0;//counter for the letters
-    for (int i = 0; i < y; i++)
+    if (i == y)
     {
+        return;
+    }
+    else
+    {
+        //given that each sentence ends with either '.' or '!' or '?'
+        if (x[i] == '.' || x[i] == '!' || x[i] == '?')
+        {
+            (*sentence)++;
+            //printf("Sentence(s) = %d\n", *sentence);
+        }
         //checks is the character is an alphabet or not
         if (isalpha(x[i]))
         {
-            letter++;
+            (*letter)++;
+            //printf("letter(s) = %d\n", *letter);
         }
-    }
-    return letter;
-}
-//x holds the string s
-//y holds the length of string
-int count_word(string x, int y)
-{
-    int words = 0;//counter for the words
-    for (int i = 0; i < y; i++)
-    {
         //given that words are separated with one space
         if (x[i] == ' ')
         {
-            words++;
+            (*words)++;
+            //printf("Word(s) = %d\n", *words);
+            i++;
+            count(x, y, letter, words, sentence);
         }
         else
         {
             if (i == y - 1)
             {
-                words++;
+                (*words)++;
+                //printf("Word(s) = %d\n", *words);
+                i++;
+                count(x, y, letter, words, sentence);
+            }
+            else
+            {
+                i++;
+                count(x, y, letter, words, sentence);
             }
         }
     }
-    return words;
-}
-//x holds the string s
-//y holds the length of string
-int count_sentence(string x, int y)
-{
-    int sentence = 0;//counter for the sentences
-    for (int i = 0; i < y; i++)
-    {
-        //given that each sentence ends with either '.' or '!' or '?'
-        if (x[i] == '.' || x[i] == '!' || x[i] == '?')
-        {
-            sentence++;
-        }
-    }
-    return sentence;
+
 }
 //k holds the number of letters
 //l holds the number of words
 //m holds the number of sentences
-void get_grade(int k, int l, int m)
+void get_grade(int *k, int *l, int *m)
 {
-    float L = ((float)k * 100) / l;//L is the average number of letters per 100 words
-    float S = ((float)m * 100) / l;//S is the average number of sentences per 100 words
+    float L = ((float)(*k) * 100) / (*l);//L is the average number of letters per 100 words
+    float S = ((float)(*m) * 100) / (*l);//S is the average number of sentences per 100 words
     int index = round((0.0588 * L) - (0.296 * S) - 15.8);//index holds the grade level
     if (index >= 16)
     {
